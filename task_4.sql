86b1271ffd2bdb7e05d5000dd4eeb9022271dd2b
abel-hak <hakensoabel@gmail.com>
2024-10-20 17:14:42 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,13 @@
--- Show the full description of the table books
-SHOW CREATE TABLE Books;
+-- Query to retrieve the full description of the Books table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
